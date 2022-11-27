f542b2e2a0a9215feabc8c0ef619a34db6749769
Ben <ben.kelly@gmx.ch>
2022-11-27 15:43:38 +0100

    + porcupine setup & Loading animation during

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:async';
 
-import 'package:porcupine_flutter/porcupine.dart';
 import 'package:porcupine_flutter/porcupine_manager.dart';
 import 'package:porcupine_flutter/porcupine_error.dart';
 
@@ -17,7 +16,6 @@ class _HomeViewState extends State<HomeView> {
   bool _isListening = false;
   bool _isInitializing = true;
   PorcupineManager? _porcupineManager;
-  String? _porcupineAccessKey;
 
   String getSuggestion() =>
       (!_isListening) ? "Press the button" : "Say \"Android ascend\"";
@@ -25,66 +23,78 @@ class _HomeViewState extends State<HomeView> {
   @override
   void initState() {
     super.initState();
-    dotenv.load().then((_) async {
-      _porcupineAccessKey = dotenv.maybeGet('PORCUPINE_ACCESS_KEY');
-      if (_porcupineAccessKey == null) return;
-      try {
-        _porcupineManager = await PorcupineManager.fromBuiltInKeywords(
-            _porcupineAccessKey!,
-            [BuiltInKeyword.PORCUPINE, BuiltInKeyword.BUMBLEBEE],
-            _wakeWordCallback);
-      } on PorcupineException catch (err) {
-        // handle porcupine init error
+    dotenv.load(fileName: 'assets/.env').then((_) async {
+      String? porcupineAccessKey = dotenv.maybeGet('PORCUPINE_ACCESS_KEY');
+      if (porcupineAccessKey != null) {
+        await initPorcupine(porcupineAccessKey);
       }
-    }).then((_) =>
-        setState(() => {
-        _isInitializing = false,
-        })
-    );
+    }).then((_) => setState(() => {
+          _isInitializing = false,
+        }));
   }
 
-  void createPorcupineManager() async {
-    if (_porcupineAccessKey == null) {
-      return;
+  Future<void> initPorcupine(String accessKey) async {
+    try {
+      _porcupineManager = await PorcupineManager.fromKeywordPaths(
+          accessKey,
+          ["assets/porcupine/android-ascend_en_android_v2_1_0.ppn"],
+          _wakeWordCallback);
+    } on PorcupineException catch (err) {
+      showMessage(err.toString());
     }
+  }
+
+  void showMessage(String message) {
+    SnackBar snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   void _wakeWordCallback(int keywordIndex) {
     if (keywordIndex == 0) {
-      // porcupine detected
-    }
-    else if (keywordIndex == 1) {
-      // bumblebee detected
+      showMessage("android ascend detected");
     }
   }
 
-  void detectWakeWord() {
+  Future<void> detectWakeWord() async {
     setState(() {
       _isListening = true;
     });
-    Future.delayed(const Duration(seconds: 3)).then((value) =>
-        setState(() {
-          _isListening = false;
-        }));
+    try {
+      await _porcupineManager!.start();
+    } on PorcupineException catch (ex) {
+      showMessage(ex.toString());
+    }
+    Future.delayed(const Duration(seconds: 3)).then((value) => {
+          _porcupineManager!.stop().then((_) => {
+                setState(() {
+                  _isListening = false;
+                })
+              })
+        });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 10.0),
-          child: CircleAvatar(
-            backgroundColor: Colors.blue,
-            child: GestureDetector(
-              onTap: detectWakeWord,
-              child: const Icon(Icons.record_voice_over_rounded),
-            ),
-          ),
-        ),
-        Text(getSuggestion()),
-      ],
-    );
+    return (_isInitializing)
+        ? const Center(
+            child: CircularProgressIndicator(
+            color: Colors.grey,
+          ))
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
+                child: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: GestureDetector(
+                    onTap: detectWakeWord,
+                    child: const Icon(Icons.record_voice_over_rounded),
+                  ),
+                ),
+              ),
+              Text(getSuggestion()),
+            ],
+          );
   }
 }
