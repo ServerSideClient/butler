edc4742f845112527266090d8ed33414731f3b71
Ben <ben.kelly@gmx.ch>
2022-11-27 14:10:13 +0100

    Inserted record button (w/o functionality) with suggestion into home

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -8,8 +9,37 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  bool isListening = false;
+
+  String getSuggestion() =>
+      (!isListening) ? "Press the button" : "Say \"Android ascend\"";
+
+  void detectWakeWord() {
+    setState(() {
+      isListening = true;
+    });
+    Future.delayed(const Duration(seconds: 3)).then((value) => setState(() {
+          isListening = false;
+        }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(vertical: 10.0),
+          child: CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: GestureDetector(
+              onTap: detectWakeWord,
+              child: const Icon(Icons.record_voice_over_rounded),
+            ),
+          ),
+        ),
+        Text(getSuggestion()),
+      ],
+    );
   }
 }
