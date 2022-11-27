85fdf0e6ec668c1b961ca20d40be9d54dbb61799
Ben <ben.kelly@gmx.ch>
2022-11-27 10:35:15 +0100

    + Centered parameter for DefaultLayout

--- lib/layouts/default_layout.dart
+++ lib/layouts/default_layout.dart
@@ -4,8 +4,9 @@ class DefaultLayout extends StatefulWidget {
 
   final String title;
   final Widget body;
+  final bool centered;
 
-  const DefaultLayout({Key? key, required this.title, required this.body}) : super(key: key);
+  const DefaultLayout({Key? key, required this.title, required this.body, this.centered = true}) : super(key: key);
 
   @override
   State<DefaultLayout> createState() => _LayoutState();
@@ -14,11 +15,20 @@ class DefaultLayout extends StatefulWidget {
 class _LayoutState extends State<DefaultLayout> {
   @override
   Widget build(BuildContext context) {
+    Widget bodyToDisplay;
+    if (widget.centered) {
+      bodyToDisplay = Center(
+        child: widget.body,
+      );
+    }
+    else {
+      bodyToDisplay = widget.body;
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: widget.body,
+      body: bodyToDisplay,
     );
   }
 }
