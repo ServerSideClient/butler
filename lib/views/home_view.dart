207eb4bef2750ea78351368aeecbb7a5c999a856
ServerSideClient <Benjamin.Kelly@gmx.net>
2023-08-13 10:33:12 +0200

    master: Refactored HomeView to use Services + showing errors as MaterialBanners

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:mobile_assistant/models/inference_intent.dart';
+import 'package:mobile_assistant/services/intent_listener_service.dart';
+import 'package:mobile_assistant/services/weather_service.dart';
 import 'dart:async';
 
-import 'package:porcupine_flutter/porcupine_manager.dart';
-import 'package:porcupine_flutter/porcupine_error.dart';
+import '../services/alarm_service.dart';
+import '../services/wake_word_service.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -14,106 +16,120 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  bool _isListening = false;
-  bool _isInitializing = true;
-  PorcupineManager? _porcupineManager;
+  final AlarmService _alarmService = AlarmService();
+  final WeatherService _weatherService = WeatherService();
+  late final WakeWordService _wakeWordService;
+  late final IntentListenerService _intentService;
 
-  String getSuggestion() =>
-      (!_isListening) ? "Press the button" : "Say \"Android ascend\"";
-
-  @override
-  void initState() {
-    super.initState();
-    dotenv.load(fileName: 'assets/.env').then((_) async {
-      String? porcupineAccessKey = dotenv.maybeGet('PORCUPINE_ACCESS_KEY');
-      if (porcupineAccessKey == null) throw Exception('Access key missing in env file.');
-      await initPorcupine(porcupineAccessKey);
-    }).onError((error, stackTrace) {
-      showMessage('Error occurred reading api key: $error');
-    }).then((_) => setState(() => {
-          _isInitializing = false,
-        }));
+  _HomeViewState() {
+    _wakeWordService = WakeWordService(
+      onWordDetected: () async => await detectIntent(),
+    );
+    _intentService = IntentListenerService(onIntent: _processIntent);
+    _alarmService.onError = showError;
+    _weatherService.onError = showError;
+    _wakeWordService.onError = showError;
+    _intentService.onError = showError;
+    _alarmService.onInfo = showInfo;
+    _weatherService.onInfo = showInfo;
+    _wakeWordService.onInfo = showInfo;
+    _intentService.onInfo = showInfo;
   }
 
   @override
-  void dispose() {
-    _porcupineManager?.delete().whenComplete(() => {
-      super.dispose()
-    });
+  Future<void> dispose() async {
+    await _alarmService.dispose();
+    await _wakeWordService.dispose();
+    await _intentService.dispose();
+    super.dispose();
   }
 
-  Future<bool> requestRecordingPermissions() async {
-    if (await Permission.microphone.request().isGranted == false) {
-      showMessage("Button won't work without this permission being granted.");
+  Future<bool> boot() async {
+    await dotenv.load(fileName: 'assets/.env');
+    String? voiceAccessKey = dotenv.maybeGet('VOICE_ACCESS_KEY');
+    if (voiceAccessKey != null) {
+      _wakeWordService.accessKey = voiceAccessKey;
+      _intentService.accessKey = voiceAccessKey;
+      await _wakeWordService.init();
+      await _intentService.init();
+      return true;
+    } else {
+      showInfo("Access key for Porcupine/Rhino is missing");
       return false;
     }
-    return true;
   }
 
-  Future<void> initPorcupine(String accessKey) async {
-    try {
-      _porcupineManager = await PorcupineManager.fromKeywordPaths(
-          accessKey,
-          ["assets/porcupine/android-ascend_en_android_v2_1_0.ppn"],
-          _wakeWordCallback);
-    } on PorcupineException catch (err) {
-      showMessage(err.toString());
-    }
-  }
-
-  void showMessage(String message) {
+  void showInfo(String message) {
     SnackBar snackBar = SnackBar(content: Text(message));
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    if (context.mounted) ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
-  void _wakeWordCallback(int keywordIndex) {
-    print("KeywordIndex: $keywordIndex");
-    if (keywordIndex == 0) {
-      showMessage("Wake word detected");
+  void showError(String message) {
+    MaterialBanner banner =
+        MaterialBanner(content: Text(message), actions: const [
+      TextButton(
+        onPressed: null,
+        child: Text('DISMISS'),
+      )
+    ]);
+    if (context.mounted) {
+      ScaffoldMessenger.of(context).showMaterialBanner(banner);
     }
   }
 
-  Future<void> detectWakeWord() async {
-    if (await requestRecordingPermissions() == false) return;
-    setState(() {
-      _isListening = true;
-    });
-    try {
-      await _porcupineManager!.start();
-    } on PorcupineException catch (ex) {
-      showMessage(ex.toString());
+  Future<void> _processIntent(
+      InferenceIntent intent, Map<String, String>? slots) async {
+    switch (intent) {
+      case InferenceIntent.alarm:
+        await _alarmService.process(slots);
+        break;
+      case InferenceIntent.weather:
+        await _weatherService.process(slots);
+        break;
     }
-    Future.delayed(const Duration(seconds: 3)).then((value) => {
-          _porcupineManager!.stop().then((_) => {
-                setState(() {
-                  _isListening = false;
-                })
-              })
-        });
+  }
+
+  Future<void> detectIntent() async {
+    await _wakeWordService.stopListening();
+    await _intentService.listen();
   }
 
   @override
   Widget build(BuildContext context) {
-    return (_isInitializing)
-        ? const Center(
-            child: CircularProgressIndicator(
+    return FutureBuilder<bool>(
+      future: boot(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          bool errorFree = snapshot.data ?? false;
+          return (errorFree)
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(vertical: 10.0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.blue,
+                        child: GestureDetector(
+                          onTap: () async => await _wakeWordService.listen(),
+                          child: const Icon(Icons.record_voice_over_rounded),
+                        ),
+                      ),
+                    ),
+                    ValueListenableBuilder(
+                        valueListenable: _wakeWordService.isListening,
+                        builder: (c, listening, _) => Text((!listening)
+                            ? "Press the button"
+                            : "Say \"Android awaken\"")),
+                  ],
+                )
+              : const Placeholder();
+        } else {
+          return const Center(
+              child: CircularProgressIndicator(
             color: Colors.grey,
-          ))
-        : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 10.0),
-                child: CircleAvatar(
-                  backgroundColor: Colors.blue,
-                  child: GestureDetector(
-                    onTap: detectWakeWord,
-                    child: const Icon(Icons.record_voice_over_rounded),
-                  ),
-                ),
-              ),
-              Text(getSuggestion()),
-            ],
-          );
+          ));
+        }
+      },
+    );
   }
 }
