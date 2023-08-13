99cad607d50d05465f6b14cf07fc4290dec6aa8b
ServerSideClient <Benjamin.Kelly@gmx.net>
2023-08-13 10:17:02 +0200

    master: Defined enums of slots in rhino context v1

---
+++ lib/models/slots.dart
@@ -0,0 +1,16 @@
+enum Day {
+  today(Duration()),
+  tomorrow(Duration(days: 1));
+
+  const Day(this.durationFromToday);
+
+  final Duration durationFromToday;
+
+  static Day? fromString(String string) {
+    try {
+      return values.firstWhere((e) => e.name == string);
+    } on StateError {
+      return null;
+    }
+  }
+}
