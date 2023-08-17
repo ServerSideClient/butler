8b3c7320160871dd21a0fcf59245a8faa1ef2a9f
ServerSideClient <Benjamin.Kelly@gmx.net>
2023-08-17 16:45:27 +0200

    feature/logging: + FilteredRotatingFileAppender

--- lib/utils/logging.dart
+++ lib/utils/logging.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
+import 'package:logging_appenders/logging_appenders.dart';
+
+class FilteredRotatingFileAppender extends RotatingFileAppender {
+  final Level filterLevel;
+
+  FilteredRotatingFileAppender({required super.baseFilePath, super.formatter, super.clock, super.keepRotateCount, super.rotateAtSizeBytes, super.rotateCheckInterval, this.filterLevel = Level.ALL});
+  @override
+  void handle(LogRecord record) {
+    if (filterLevel <= record.level) {
+      super.handle(record);
+    }
+  }
+}
 
 mixin Logging {
   @protected
