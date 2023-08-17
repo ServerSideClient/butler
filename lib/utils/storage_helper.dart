1f94bdf01222481d9f85ef0ff16289ae981bfb51
ServerSideClient <Benjamin.Kelly@gmx.net>
2023-08-17 16:45:49 +0200

    feature/logging: + StorageHelper

---
+++ lib/utils/storage_helper.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:butler/utils/logging.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+class StorageHelper with Logging {
+  static final StorageHelper _instance = StorageHelper._internal();
+  factory StorageHelper.getInstance() => _instance;
+  StorageHelper._internal();
+
+  Directory? _logsDir;
+  final String logName = "logs.txt";
+
+  Future<void> init() async {
+    _logsDir = Directory(join((await getApplicationDocumentsDirectory()).path, "logs"));
+  }
+
+  void _throwNotInitialisedIfNull(Object? value) {
+    if (value == null) {
+      throw Exception("StorageHelper not initialised.");
+    }
+  }
+
+  Directory get logsDir {
+    _throwNotInitialisedIfNull(_logsDir);
+    return _logsDir!;
+  }
+}
