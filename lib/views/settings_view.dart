ab0a6d742a3e9f337592dfa5f63793d59c473005
ServerSideClient <Benjamin.Kelly@gmx.net>
2023-08-19 14:07:18 +0200

    feature/weather-change-location: + "Location" Setting row

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -1,16 +1,26 @@
 import 'package:butler/layouts/default_layout.dart';
+import 'package:butler/utils/shared_preferences_helper.dart';
+import 'package:butler/views/dialogs/location_setting_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatelessWidget with SharedPreferencesAccess {
   const SettingsView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
+    return DefaultLayout(
       title: "Settings",
+      centered: false,
       topPadding: 32.0,
       horizontalPadding: 16,
-      children: [],
+      children: [
+        _buildSettingsRow(
+            "Location",
+            ElevatedButton(
+                onPressed: () async => await _showLocationChangeDialog(context),
+                child: const Text("Change")))
+      ],
     );
   }
 
@@ -22,5 +32,24 @@ class SettingsView extends StatelessWidget {
         children: [Text(label), settingWidget],
       ),
     );
+  }
+
+  Future<void> _showLocationChangeDialog(BuildContext context) async {
+    var currentAddress =
+        prefs.getString(SharedPreferencesHelper.keySettingAddress);
+    await showDialog(
+        context: context,
+        builder: (_) => LocationSettingDialog(
+              onSetLocation: _setLocationSetting,
+              initialLocation: currentAddress,
+            ));
+  }
+
+  Future<void> _setLocationSetting(Location location, String address) async {
+    await prefs.setString(SharedPreferencesHelper.keySettingAddress, address);
+    await prefs.setDouble(
+        SharedPreferencesHelper.keySettingLongitude, location.longitude);
+    await prefs.setDouble(
+        SharedPreferencesHelper.keySettingLatitude, location.latitude);
   }
 }
