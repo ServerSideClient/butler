99483da62d2cff4455e136a668ae2ea4c0f65619
Benjamin Kelly <Ben.Kelly@gmx.ch>
2023-09-17 14:08:20 +0200

    feature/alarm: awaiting _setAlarm

--- lib/services/alarm_service.dart
+++ lib/services/alarm_service.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:alarm/alarm.dart';
+import 'package:butler/utils/shared_preferences_helper.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_system_ringtones/flutter_system_ringtones.dart';
 import 'package:butler/models/slots.dart';
 import 'package:butler/services/service.dart';
+import 'package:butler/utils/list_extensions.dart';
 
-class AlarmService extends IntentService {
+class AlarmService extends IntentService with SharedPreferencesAccess {
   @override
   Future<void> init() async {
     await Alarm.init();
@@ -19,7 +25,7 @@ class AlarmService extends IntentService {
       }
       int hour = int.tryParse(slots["hour"]!) ?? -1;
       if (hour >= 0) {
-        _setAlarm(day, hour);
+        await _setAlarm(day, hour);
       } else {
         doOnError("Failed to understand when the alarm should be set for.");
       }
@@ -43,10 +49,8 @@ class AlarmService extends IntentService {
     if (alarms.isNotEmpty) {
       maxId = alarms.map((e) => e.id).reduce((e, max) => (e > max) ? e : max);
     }
-    var ringtone = ((await FlutterSystemRingtones.getAlarmSounds())
-            .firstOrNull) ??
-        ((await FlutterSystemRingtones.getRingtoneSounds()).firstOrNull) ??
-        ((await FlutterSystemRingtones.getNotificationSounds()).firstOrNull);
+
+    var ringtone = await _getRandomRingtone();
     if (ringtone == null) {
       doOnInfo("Cancelled: No audio tracks found");
       return;
@@ -54,7 +58,7 @@ class AlarmService extends IntentService {
     try {
       if (await Alarm.set(
           alarmSettings: AlarmSettings(
-              id: ++maxId, dateTime: dateTime, assetAudioPath: ringtone.uri))) {
+              id: ++maxId, dateTime: dateTime, assetAudioPath: ringtone))) {
         doOnInfo("Alarm set for ${day.toString()} at $hour.");
       } else {
         doOnError("Failed: Unknown error occurred by setting an alarm.");
@@ -62,5 +66,41 @@ class AlarmService extends IntentService {
     } on AlarmException catch (e) {
       doOnError("Failed: ${e.message}");
     }
+  }
+
+  Future<String?> _getRandomRingtone() async {
+    var ringtoneDirs = prefs.getStringList(
+            SharedPreferencesHelper.keySettingRingtoneDirectory) ??
+        [];
+    String? ringtonePath;
+    if (ringtoneDirs.isEmpty) {
+      final manifestJson = await rootBundle.loadString('AssetManifest.json');
+      var ringtoneAssets = json
+          .decode(manifestJson)
+          .keys
+          .where((key) => key.startsWith('assets/ringtones/'));
+      if (ringtoneAssets is List<String>) {
+        var ringtones = ringtoneAssets.cast<String>();
+        if (ringtones.isEmpty) {
+          ringtonePath =
+              (((await FlutterSystemRingtones.getAlarmSounds()).firstOrNull) ??
+                      ((await FlutterSystemRingtones.getRingtoneSounds())
+                          .firstOrNull) ??
+                      ((await FlutterSystemRingtones.getNotificationSounds())
+                          .firstOrNull))
+                  ?.uri;
+        } else {
+          ringtonePath = ringtones.randomPick();
+        }
+      }
+    } else {
+      ringtonePath = await Directory(ringtoneDirs.randomPick()!)
+          .list()
+          .map((event) => event.path)
+          .where((element) => element.endsWith(".mp3"))
+          .toList()
+          .then((value) => value.randomPick());
+    }
+    return ringtonePath;
   }
 }
